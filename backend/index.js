3f3483da912934d8e947c11871c9d0fc9fc56f8b
Lena Kim <lenakim0215@gmail.com>
2022-03-05 17:05:47 -0800

    firebase

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,81 @@
-import { registerRootComponent } from 'expo';
+var admin = require("firebase-admin");
+var serviceAccount = require("/Users/lena/Desktop/cmd-f2022-firebase-adminsdk-8o63m-fa0cb3449b.json");
 
-import App from './App';
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+  
+const db = admin.firestore();
+let userRef = db.collection("Users");
+let groupRef = db.collection("Groups");
+let postRef = db.collection("Posts");
+let commentRef = db.collection("Comments");
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+function createUser(email, name, pwd, origin, res) {
+    const groupName = origin + "-" + res;
+    userRef.add({
+            email: email,
+            name: name,
+            password: pwd,
+            origin: origin,
+            residence: res,
+            group: groupName
+    });
+    console.log("User created successfully");
+
+    const doc = groupRef.doc(groupName).get();
+    if (!doc.exists) {
+        groupRef.doc(groupName).set({});
+        console.log("Group created successfully");
+    }
+
+    // TO DO: send the userID and groupID to the frontend
+}
+
+//create a post function
+function createPost(userID, groupID, content) {
+    // create a new post document (object)
+    postRef.add({
+      user: userID,
+      content: content, 
+      timestamp: admin.firestore.Timestamp.now()
+    })
+    // get the postID and add the post to the group
+    .then(function(postRef) {
+      const postID = postRef.id;
+      groupRef.doc(groupID).update({
+          posts: admin.firestore.FieldValue.arrayUnion(postID)
+      });
+    })
+    // TO DO: send the postID to React
+}
+
+// create a comment document
+function createComment(userID, postID, content) {
+    // create a new comment object
+    commentRef.add({
+      user: userID,
+      content: content,
+          timestamp: admin.firestore.Timestamp.now() 
+    })  
+    // add the commentID to the post
+    .then(function(commentRef) {
+        const commentID = commentRef.id;
+        postRef.doc(postID).update({
+        comments: admin.firestore.FieldValue.arrayUnion(commentID)
+      });
+    })
+}  
+
+////////////////// test //////////////////
+console.log(admin.firestore.Timestamp.now());
+createPost("test", "A-B", "conttt");
+createComment("userID", "MOGJrtJDLb24YCocQWWR", "testcomment")
+
+// view the info for testing
+userRef.get().then((querySnapshot) => {
+    querySnapshot.forEach(document => {
+        console.log(document.data());
+    })
+})
+////////////////////////////////////

1e5ff734827d11b5ea05cb0fb9ec63ead3ba2b46
Lena Kim <lenakim0215@gmail.com>
2022-03-06 02:11:05 -0800

    router

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,30 @@
-const database = require("./firebase");
-const mapview = require("./maps");
+import express from 'express';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 
+//const database = require("./firebase.js");
+//const mapview = require("./maps.js");
+
+const PORT = process.env.PORT || 3001;
 const app = express();
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+app.use("/", router);
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+router.get('/',(req, res) => {
+    res.send('hey');
+});
+
+router.post('/signup',(req, res) => {
+    var username = req.body.name;
+    var password = req.body.password;
+    res.end("yes");
+});
+
+app.listen( PORT, () => {
+    console.log('Server listening');
+});
